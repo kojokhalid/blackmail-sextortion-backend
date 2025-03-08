afc19112ba8f7d8713d4c4975f8e6641eb5eace7
Antoh SHadrack <129493093+kojokhalid@users.noreply.github.com>
2025-03-08 18:48:28 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express();
 const port = 8000;
 const userRoutes = require("./modules/users/user.routes");
 const chatRoutes = require("./modules/chats/chat.routes");
+const feedbackRoutes = require("./modules/feedback/feedback.routes")
 const errorHandler = require("./handlers/errorHandler");
 const fetchTweet = require("./modules/twitter/fetchTweet");
 app.use(express.json());
@@ -22,16 +23,17 @@ mongoose
 require("./models/users.model");
 require("./models/otp.model");
 require("./models/chat.model");
+require("./models/feedback.model");
 app.use("/api/user", userRoutes);
 app.use("/api/chat",chatRoutes );
 app.get("/api/fetchtweets", fetchTweet);
-
-app.all("*",(req,res,next)=>{
-  res.status(404).json({
-    status:"failed",
-    message:"not found"
-  })
-})
+app.use("/api/feedback",feedbackRoutes)
+// app.all("*",(req,res,next)=>{
+//   res.status(404).json({
+//     status:"failed",
+//     message:"not found"
+//   })
+// })
 // error handler
 app.use(errorHandler);
 app.listen(port, () => console.log(`app listening on port ${port}!`));
